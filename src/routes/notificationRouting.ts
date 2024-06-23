773ae6ed20c83417f84dc7c97377aed6fb0d7de3
mahmoudmohamed <mmaahm2002@gmail.com>
2024-06-23 19:05:40 +0300

    "Added new routes for updating and deleting notifications, and added route parameter for deleting a single notification"

--- src/routes/notificationRouting.ts
+++ src/routes/notificationRouting.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { protectionAuth } from '../middlewares/verifyAuth';
-import { fetchNotifications } from '../controllers/notificationController';
+import { deleteAllNotifications, deleteNotification, fetchNotifications, updateNotifications } from '../controllers/notificationController';
 
 
 const router = express.Router();
 
-router.get('/notification', protectionAuth, fetchNotifications)
+router.route('/notification')
+    .get(protectionAuth, fetchNotifications)
+    .put(protectionAuth, updateNotifications)
+    .delete(protectionAuth, deleteAllNotifications)
+
+router.route('/notification/:id')
+    .delete(deleteNotification)
 
 
 export default router
