229c0b13ff9d7d54219b065dd836abda5d90ba5e
mahmoudmohamed <mmaahm2002@gmail.com>
2024-07-02 16:11:47 +0300

    "Updated cookie name from 'auth_token' to 'token' in logout route"

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/logout', async (req: Request, res: Response) => {
     try {
-        res.clearCookie('auth_token');
+        res.clearCookie('token');
         res.status(200).json({ message: "Logged out" });
     } catch (error: any) {
         return res.status(400).json({ message: 'There is Error', error: error.message })
